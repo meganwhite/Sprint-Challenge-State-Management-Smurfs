d5acecab438872bc5de729ab4f0a5b177038daaa
Megan White <mfw247@gmail.com>
2019-08-16 09:36:22 -0700

    added form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Smurfs from './Smurfs'
+import FormikUserForm from './SmurfForm'
 
 import "./App.css";
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
         <Smurfs />
+        <FormikUserForm />
       </div>
     );
   }
